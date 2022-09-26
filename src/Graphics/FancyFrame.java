f1fb073b6fea6eb943bc6bbafe989b720650a518
Noah <noahcs2002@gmail.com>
2022-09-26 14:51:11 -0500

    Beginning to implement the subscriber network

--- src/Graphics/FancyFrame.java
+++ src/Graphics/FancyFrame.java
@@ -1,26 +1,29 @@
 package Graphics;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 // import java.awt.event.*;
+// import java.awt.event.*;
 
-public class FancyFrame extends JFrame
+import Interfaces.ISubscriber;
+
+public class FancyFrame extends JFrame implements ISubscriber
 {
     JPanel mainPanel = new JPanel();
-    Navbar navbar = new Navbar();
+    Navbar navbar = new Navbar(this);
 
 
     public FancyFrame()
     {
         mainPanel.setLayout(new BorderLayout());
         mainPanel.add(navbar, BorderLayout.NORTH);
+        mainPanel.add(new PitchingPanel("LA Dodgers"), BorderLayout.CENTER);
 
         this.add(mainPanel);
         
-        this.setSize(1000, 500);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
         this.setDefaultCloseOperation(3);
-        // this.pack();
+        this.pack();
         this.setVisible(true);
         this.setTitle("Baseball Team Manager");
         this.setLocationRelativeTo(null);
