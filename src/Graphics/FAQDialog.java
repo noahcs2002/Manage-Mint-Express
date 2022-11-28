004507815c575e8ae38767f2e46cd9be32ccd5e0
noahcs2002 <84613649+noahcs2002@users.noreply.github.com>
2022-11-27 20:46:33 -0600

    Removing games from the feature list.

--- src/Graphics/FAQDialog.java
+++ src/Graphics/FAQDialog.java
@@ -17,10 +17,8 @@ public class FAQDialog extends JDialog
             "1. To add a player, click 'Add Player' in the navbar, then select your type. " +
             "Then, fill in ALL THE FIELDS, and hit tab one time after entering the last value, then hit confirm.",
             "2. Hitting 'Clear All' will clear all entries OF THAT SPECIFIC POSITION. Clear All while on pitchers clears pitchers etc.",
-            "3. To record a game, simply open the past games dialog and enter in the team you played against, and the date of the game. (Note: "
-            + "You cannot record a game that was not scheduled. To do this, schedule the game, then record it).",
-            "4. If you have issues adding a player, make sure all values are full, and then go to the last column and hit tab, then the confirm button.",
-            "5. Inj. and Sus. MUST be entered as true or false."
+            "3. If you have issues adding a player, make sure all values are full, and then go to the last column and hit tab, then the confirm button.",
+            "4. Inj. and Sus. MUST be entered as true or false."
         };
 
         JList<String> list = new JList<>(faqs);
